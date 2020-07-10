0e32158abe6e20231cd3c898efc23fd358e9eff4
victor tran <victor.tran@tempestas.io>
2020-07-10 13:13:23 -0400

    removed self.size

--- queue/queue.py
+++ queue/queue.py
@@ -39,7 +39,6 @@ from singly_linked_list.singly_linked_list import LinkedList
 
 class Queue:
     def __init__(self):
-        self.size = 0
         self.storage = LinkedList()
     
     def __len__(self):

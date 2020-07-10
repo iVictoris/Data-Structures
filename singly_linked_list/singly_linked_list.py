43c8aeecfdd2008d823e348573f55fabcc5ee4fc
victor tran <victor.tran@tempestas.io>
2020-07-10 13:10:07 -0400

    some formatting

--- singly_linked_list/singly_linked_list.py
+++ singly_linked_list/singly_linked_list.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, data):
         self.data = data
-        self.next = None # supposed to be a node
+        self.next = None  # supposed to be a node
 
     def get_next(self):
         return self.next
@@ -11,13 +11,13 @@ class Node:
 
     def remove_next(self):
         self.next = None
-    
+
     def __str__(self):
         return f"{self.data}"
 
     def __eq__(self, value):
         return self.data == value
-    
+
     def __gt__(self, node):
         return self.data > node.data
 
@@ -28,25 +28,28 @@ class LinkedList:
         self.__head = start
         self.length = 1 if start else 0
 
+    def __len__(self):
+        return self.length
+        
     @property
     def head(self):
         return self.__head
-    
+
     @head.setter
     def head(self, data=None):
         # honestly this is probably only for setting head initially
         node = Node(data) if data else None
-        
+
         # if self.__head:
-        #     # there's a head already 
+        #     # there's a head already
         #     self.__head.set_next(node)
         # print(f'new head: {node}')
-        self.__head = node    
-        
+        self.__head = node
+
     @property
     def tail(self):
         return self.__tail
-    
+
     @tail.setter
     def tail(self, data=None):
         node = Node(data) if data else data
@@ -60,14 +63,13 @@ class LinkedList:
             self.__tail = node
             return
         # when tail already exists do the following
-        self.__tail.set_next(node) # this should connect the tail to the next node
+        # this should connect the tail to the next node
+        self.__tail.set_next(node)
         self.__tail = node
-
-
 
     def append(self, data):
         """
-        Adds a new node to the tail, sets a pointer from previous tail to next tail
+        Adds a new node to the tail or self.length + 1, sets a pointer from previous tail to next tail
         """
         # adds to tail
 
@@ -79,10 +81,9 @@ class LinkedList:
         self.tail = data
         self.add_length(1)
 
-    
     def pop(self):
         """
-        Removes current head from the linked list if there is one present
+        Removes current head or position 0 from the linked list if there is one present
         """
         # case 1: no data
         # case 2: 1 item
@@ -101,12 +102,13 @@ class LinkedList:
         self.head = self.head.get_next()
         self.add_length(-1)
         return node
-        
+
     def traverse(self):
         node = self.head
         while node:
             node = node.get_next()
-    
+
+
     def get_max(self):
         high = self.head
         node = self.head

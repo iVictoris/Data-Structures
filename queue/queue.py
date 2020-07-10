c3bd1d650fe7378d77bfc413bd447af051be930b
victor tran <victor.tran@tempestas.io>
2020-07-10 13:12:31 -0400

    add comments for thinking

--- queue/queue.py
+++ queue/queue.py
@@ -10,6 +10,27 @@ return elements in First In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Queue?
 """
+
+"""
+Let me make it easier to explain LIFO
+Think of dishes
+
+you stack them on top of each other
+and remove them based on the top of each other
+
+it's the last dish in that's the first dish out
+
+vs FIFO
+
+think of groceries
+older is in the back
+newer is in the front
+
+employees stock new in the back
+people buy items in the front
+
+new in, old out
+"""
 class Queue:
     def __init__(self):
         self.size = 0

c66207e18624a41285314c66e72ef9790ef7eb39
victor tran <victor.tran@tempestas.io>
2020-07-10 13:08:34 -0400

    write stack class

--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,45 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+
+
+"""
+Let me make it easier to explain LIFO
+Think of dishes
+
+you stack them on top of each other
+and remove them based on the top of each other
+
+it's the last dish in that's the first dish out
+
+vs FIFO
+
+think of groceries
+older is in the back
+newer is in the front
+
+employees stock new in the back
+people buy items in the front
+
+first <blank> in, first <blank> out
+"""
+
+import sys
+sys.path.append('..')
+
+from singly_linked_list.singly_linked_list import LinkedList
+
+
 class Stack:
     def __init__(self):
-        self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
 
     def __len__(self):
-        pass
+        return (len(self.storage))
+
 
     def push(self, value):
-        pass
+        self.storage.append(value)
 
     def pop(self):
-        pass
+        return self.storage.stack_pop()
